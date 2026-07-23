97ec008fd905d8db8bd8a14d987e7140625d6072
Evandro Passaia <evandrobaraka@gmail.com>
2026-07-23 15:15:02 -0300

    build: ORM Prisma atualizado para a versão 7

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,5 @@
 import "dotenv/config";
 import { prisma } from "../src/utils/prisma";
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-// const prisma = new PrismaClient({
-//     adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
-// });
 
 const products = [
     {
